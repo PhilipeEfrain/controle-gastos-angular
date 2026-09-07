4219954fbad84401e4526537bd95baa48b2dee4a
Philipe Efrain <34327261+PhilipeEfrain@users.noreply.github.com>
2026-09-07 09:36:36 -0300

    feat(core): setup inicial angular 22, scss modular e firebase modular sdk (#2)

---
+++ src/environments/environment.production.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    projectId: 'controle-gastos-app-36264',
+    appId: '1:562686483207:web:8f8fb528069c1af4b083a2',
+    storageBucket: 'controle-gastos-app-36264.firebasestorage.app',
+    apiKey: 'AIzaSyDOg6-zXuE8W3w8Kr3Nyx_OLq7lFKBUeU0',
+    authDomain: 'controle-gastos-app-36264.firebaseapp.com',
+    messagingSenderId: '562686483207'
+  }
+};
